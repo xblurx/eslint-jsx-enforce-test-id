600b86f5ee8522b57a53875fdf9242b13b8627ea
xblurx <xblurx@protonmail.com>
2021-11-28 18:37:25 +0300

    fix webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,47 +1,30 @@
 import path from 'path';
-import webpack, { ContextReplacementPlugin } from 'webpack';
+import webpack from 'webpack';
 import TerserPlugin from 'terser-webpack-plugin';
 import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
-// @ts-ignore
 import CompressionPlugin from 'compression-webpack-plugin';
-
-const plugins = [
-    new ForkTsCheckerWebpackPlugin(),
-    new ContextReplacementPlugin(/moment[/\\]locale$/, /ru/),
-    new CompressionPlugin(),
-    new webpack.ProvidePlugin({ process: 'process/browser' }),
-];
-
-const tsLoader = (test: any) => {
-    const presets = ['@babel/preset-env', '@babel/preset-typescript'];
-    const plugins = ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-transform-runtime'];
-
-    const use = [
-        'thread-loader',
-        {
-            loader: 'babel-loader',
-            options: {
-                plugins,
-                presets,
-            },
-        },
-    ];
-
-    return {
-        test,
-        use,
-    };
-};
-
-const rules = [tsLoader(/\.(j|t)s$/)];
 
 const config = {
     entry: './src',
     optimization: { minimize: true, minimizer: [new TerserPlugin({ parallel: true })] },
     target: ['web', 'es5'],
     module: {
-        rules,
+        rules: [
+            {
+                test: /\.(j|t)s$/,
+                use: [
+                    'thread-loader',
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            plugins: ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-transform-runtime'],
+                            presets: ['@babel/preset-env', '@babel/preset-typescript'],
+                        },
+                    },
+                ],
+            },
+        ],
     },
     resolve: {
         plugins: [new TsconfigPathsPlugin()],
@@ -56,7 +39,11 @@ const config = {
         },
         clean: true,
     },
-    plugins,
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new CompressionPlugin(),
+        new webpack.ProvidePlugin({ process: 'process/browser' }),
+    ],
 };
 
 export default config;
